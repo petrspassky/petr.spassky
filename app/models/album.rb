b58722675bf435c539b50613e741ca667b1fc4ad
Dima Lunich <dima.lunich@gmail.com>
2017-08-29 21:02:41 +0200

    Album model added

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,9 @@
 class Album < ApplicationRecord
   belongs_to :site
-  belongs_to :artist
+  belongs_to :artist, optional: true
 
   has_many :album_models, dependent: :destroy
-  has_many :models, through: :album_models
+  has_many :site_models, through: :album_models
+
+  delegate :title, :url, to: :site, prefix: true
 end
