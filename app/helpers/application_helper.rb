a6bd597a5b9c507eda22a965da42c0860de8461c
Petr Spassky <petr.spassky@gmail.com>
2017-09-01 00:06:19 +0200

    Albums pagination added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def prev_url?
+    params[:offset].to_i > 0
+  end
+
+  def prev_url(per_page = 20)
+    offset = params[:offset].to_i - per_page
+    { offset: offset }
+  end
+
+  def next_url(per_page = 20)
+    offset = params[:offset].to_i + per_page
+    { offset: offset }
+  end
 end
